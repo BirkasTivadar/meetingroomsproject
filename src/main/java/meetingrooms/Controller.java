be8c6da8225043040d71e2c5363ab26a995bca64
BirkasTivadar <birkastivadar@gmail.com>
2022-11-29 14:33:46 +0100

    Tárgyalók projektfeladat alap

--- src/main/java/meetingrooms/Controller.java
+++ src/main/java/meetingrooms/Controller.java
@@ -1,2 +1,103 @@
-package meetingrooms;public class Controller {
+package meetingrooms;
+
+import java.util.Scanner;
+
+public class Controller {
+
+    private final Office office = new Office();
+
+    public void readOffice() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hány tárgyalót szeretne rögzíteni: ");
+        int numberOfMeetingrooms = scanner.nextInt();
+        scanner.nextLine();
+
+        loadOffice(scanner, numberOfMeetingrooms);
+
+
+    }
+
+    private void loadOffice(Scanner scanner, int numberOfMeetingrooms) {
+        String name;
+        int width;
+        int length;
+
+        for (int i = 1; i <= numberOfMeetingrooms; i++) {
+            System.out.printf("Kérem a(z) %d. tárgyaló nevét: %n", i);
+            name = scanner.nextLine();
+            System.out.printf("Kérem a(z) %d. tárgyaló szélességét: %n", i);
+            width = scanner.nextInt();
+            System.out.printf("Kérem a(z) %d. tárgyaló hosszúságát%n", i);
+            length = scanner.nextInt();
+            scanner.nextLine();
+            office.addMeetingRoom(new MeetingRoom(name, width, length));
+        }
+    }
+
+    public void printMenu() {
+        System.out.println("1. Tárgyalók sorrendben\n" +
+                "2. Tárgyalók visszafele sorrendben\n" +
+                "3. Minden második tárgyaló\n" +
+                "4. Területek\n" +
+                "5. Keresés pontos név alapján\n" +
+                "6. Keresés névtöredék alapján\n" +
+                "7. Keresés terület alapján\n");
+    }
+
+    public void runMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Melyik menüpontot választja?");
+        int menu = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (menu) {
+            case 1 -> office.printNames();
+            case 2 -> office.printNamesReverse();
+            case 3 -> office.printEventNames();
+            case 4 -> office.printAreas();
+            case 5 -> printWithName(scanner);
+            case 6 -> printWithContains(scanner);
+            case 7 -> printWithArea(scanner);
+
+            default -> returnToMenu();
+        }
+    }
+
+    private void printWithName(Scanner scanner) {
+        System.out.println("Kérem a keresett tárgyaló nevét:");
+        String name = scanner.nextLine();
+        office.printMeetingRoomsWithName(name);
+    }
+
+    private void printWithContains(Scanner scanner) {
+        System.out.println("Kérem a keresett tárgyaló nevének részletét:");
+        String part = scanner.nextLine();
+        office.printMeetingRoomsContains(part);
+    }
+
+    private void printWithArea(Scanner scanner) {
+        System.out.println("Kérem a terület nagyságot");
+        int area = scanner.nextInt();
+        office.printAreasLargerThan(area);
+    }
+
+    private void returnToMenu() {
+        System.out.println("Rossz menüpont");
+        printMenu();
+        runMenu();
+    }
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        controller.readOffice();
+        controller.printMenu();
+        controller.runMenu();
+
+        for (int i = 0; i < 10; i++) {
+            controller.printMenu();
+            controller.runMenu();
+        }
+    }
 }
--- src/main/java/meetingrooms/Office.java
+++ src/main/java/meetingrooms/Office.java
@@ -1,2 +1,73 @@
-package meetingrooms;public class Office {
+package meetingrooms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class Office {
+
+    private final List<MeetingRoom> meetingRooms = new ArrayList<>();
+
+    public void addMeetingRoom(MeetingRoom meetingRoom) {
+        meetingRooms.add(meetingRoom);
+    }
+
+    public void printNames() {
+        meetingRooms.forEach(meetingRoom -> System.out.println(meetingRoom.getName()));
+    }
+
+
+    public void printNamesReverse() {
+        List<MeetingRoom> meetingRoomsReverse = new ArrayList<>(meetingRooms);
+        Collections.reverse(meetingRoomsReverse);
+        meetingRoomsReverse.forEach(meetingRoom -> System.out.println(meetingRoom.getName()));
+    }
+
+    public void printEventNames() {
+        IntStream.range(0, meetingRooms.size())
+                .filter(i -> i % 2 != 0)
+                .mapToObj(meetingRooms::get)
+                .forEach(meetingRoom -> System.out.println(meetingRoom.getName()));
+    }
+
+    public void printAreas() {
+        meetingRooms.forEach(meetingRoom -> System.out.printf("A tárgyaló neve: %s, szélessége: %d, hosszúsága: %d, területe: %d%n", meetingRoom.getName(), meetingRoom.getWidth(), meetingRoom.getLength(), meetingRoom.getArea()));
+    }
+
+    public void printMeetingRoomsWithName(String name) {
+        Optional<String> meetingRoomName = meetingRooms.stream()
+                .map(MeetingRoom::getName)
+                .filter(roomName -> roomName.equals(name))
+                .findAny();
+        String result = meetingRoomName.orElse("Nincs ilyen nevű tárgyaló");
+        System.out.println(result);
+    }
+
+    public void printMeetingRoomsContains(String part) {
+        Optional<MeetingRoom> optionalMeetingRoom = meetingRooms.stream()
+                .filter(meetingRoom -> meetingRoom.getName().toLowerCase().contains(part.toLowerCase()))
+                .findAny();
+
+        MeetingRoom meetingRoom;
+        if (optionalMeetingRoom.isEmpty()) {
+            System.out.println("Nincs ilyen név részletű tárgyaló");
+            return;
+        } else {
+            meetingRoom = optionalMeetingRoom.get();
+        }
+        System.out.printf("A tárgyaló szélessége: %d, hosszűsága: %d, területe: %d%n", meetingRoom.getWidth(), meetingRoom.getLength(), meetingRoom.getArea());
+    }
+
+    public void printAreasLargerThan(int area) {
+        List<MeetingRoom> meetingRoomList = meetingRooms.stream()
+                .filter(meetingRoom -> meetingRoom.getArea() > area)
+                .toList();
+        if (meetingRoomList.isEmpty()) {
+            System.out.println("Nincs ilyen méretű tárgyaló.");
+            return;
+        }
+        meetingRoomList.forEach(meetingRoom -> System.out.printf("A tárgyaló neve: %s, szélessége: %d, hosszúsága: %d, területe: %d%n", meetingRoom.getName(), meetingRoom.getWidth(), meetingRoom.getLength(), meetingRoom.getArea()));
+    }
 }
